759cf7a5521a164c26ee96c3c630ebe880630301
Shaurya-13 <56396240+Shaurya-13@users.noreply.github.com>
2019-12-13 12:26:00 +0000

    Add files via upload

---
+++ Chime/include/navContainer.php
@@ -0,0 +1,14 @@
+﻿
+<div id="navContainer">
+        <nav class="navBar">
+
+            <a href="index.php" class="logo">
+                <img src="assets/images/icons/chimelogo.png">
+            </a>
+
+            <a href="Index.php" class="navLink">Your Music</a>
+
+        </nav>
+</div>
+
+        
